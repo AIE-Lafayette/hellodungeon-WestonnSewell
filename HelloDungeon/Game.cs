25faf811e29f66e8285f8569c5f4c6515cf9fc14
Weston Sewell <wsewell.104@gmail.com>
2023-08-23 14:53:40 -0500

    added ability to name yourself

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,7 +10,24 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            // Initializes player stats
+            string playerName = "Jim";
+            float testFloat = 3.2f;
+            float hp = 10f;
+            float atk = 10f;
+            float mp = 5f;
+            int areaNumber = 0;
+            bool playerIsAlive = hp > 0;
+            // print out name for player
+            Console.WriteLine("Weston");
+            Console.WriteLine("Sewell");
+            // get input from palyer for first and last name
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
 
+            playerName = firstName + lastName;
+            // display character name for mplayer feedback
+            Console.WriteLine("Hello " + playerName + " to me games");
         }
     }
 }
