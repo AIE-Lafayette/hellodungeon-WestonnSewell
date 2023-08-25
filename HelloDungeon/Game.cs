6264b281440aaf7e0c5d33fa5a93b3a88043f2ee
Weston Sewell <wsewell.104@gmail.com>
2023-08-25 08:50:36 -0500

    Added choices, not completely finished with this yet

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -28,6 +28,28 @@ namespace HelloDungeon
             playerName = firstName + lastName;
             // display character name for mplayer feedback
             Console.WriteLine("Hello " + playerName + " to me games");
+            // dialogue 1
+            Console.WriteLine("You run into a strange man.");
+            Console.WriteLine("He offers you a strange bottle of green fluid.");
+            Console.WriteLine("Take it..?");
+
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+            // choices and answers 1
+            string playerChoice = Console.ReadLine();
+
+            if (playerChoice == "1");
+            {
+                Console.WriteLine("Tastes like an apple.");
+                Console.WriteLine("Your skin hardens and you gain 5 HP.");
+                hp += 5;
+            }
+
+            if (playerChoice == "2");
+            {
+                Console.WriteLine("You decline the offer.");
+                Console.WriteLine("You feel oddly awful for this.");
+            }
         }
     }
 }
